b30c06893db971fe44a733fc835024339333d982
Sam Jury <sam@helpdeskcomputers.com.au>
2019-01-11 13:03:54 +1100

    localadmin

---
+++ hdclocaladmin.ps1
@@ -0,0 +1,3 @@
+﻿$Password = Read-Host -AsSecureString
+New-LocalUser "HDCLocalAdmin" -Password $Password -FullName "Help Desk Computers LocalAdmin" -Description "HelpDesk access"
+Add-LocalGroupMember -Group "Administrators" -Member "HDCLocalAdmin"
